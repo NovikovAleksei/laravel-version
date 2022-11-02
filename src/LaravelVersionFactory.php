7f6ae67565fcd540e8dd63409b085f6ccb4c1f90
Aleksei Novikov <alex.novikov@belitsoft.com>
2022-11-02 11:20:16 +0300

    modify class

--- src/LaravelVersionFactory.php
+++ src/LaravelVersionFactory.php
@@ -2,10 +2,32 @@
 
 namespace NovikovAleksei\LaravelVersion;
 
-class LaravelVersionFactory
+use Illuminate\Container\Container;
+
+class Version
 {
+    /**
+     * @var Container
+     */
+    private Container $app;
+
+    /**
+     * Version constructor.
+     *
+     * @param Container $app
+     */
+    public function __construct(Container $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Get original application version string.
+     *
+     * @return string
+     */
     public function getVersion(): string
     {
-        return 'Laravel version';
+        return $this->app->version();
     }
 }
