9936916a88d2ca00c017d401edd06335d1f2a27e
Aleksei Novikov <alex.novikov@belitsoft.com>
2022-11-08 12:16:03 +0300

    first releae

---
+++ src/VersionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NovikovAleksei\LaravelVersion;
+
+interface VersionInterface
+{
+    /**
+     * Get the version number of the application.
+     *
+     * @return string
+     */
+    public function version(): string;
+}
